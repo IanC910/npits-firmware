32fa0aaf4c2dcb35554d38217535acf9e0c1b49a
icruiksh <icruiksh@sfu.ca>
2024-07-27 03:21:27 -0700

    Add KLD2_interface_test.py

--- devices/KLD2.py
+++ devices/KLD2.py
@@ -2,14 +2,27 @@
 # This module provides interfacing methods for the K-LD2 Doppler Radar
 
 import serial
+from enum import Enum
 
-KLD2_BAUD_RATE = 38400
+class KLD2_Status(Enum):
+    OK = 0
+    ERROR = 1
+
 
 class KLD2:
-    def __init__(uart_device):
-        self.serial = serial.Serial(uart_device, KLD2_BAUD_RATE)
+    BAUD_RATE = 38400
+    RESPONSE_PREFIX = ord('@')
+
+    def __init__(self, uart_device):
+        self.serial = serial.Serial(uart_device, KLD2.BAUD_RATE)
 
     def get_sensitivity_pot_index(self):
         self.serial.write(b'$R06\r')
-        pot_index = self.serial.read(1)
-        return pot_index
+        response = self.serial.readline()
+
+        if(response[0] == KLD2.RESPONSE_PREFIX):
+            response = response.decode('utf-8')
+            pot_index = int(response[4:6])
+            return KLD2_Status.OK, pot_index
+
+        return KLD2_Status.ERROR, response
