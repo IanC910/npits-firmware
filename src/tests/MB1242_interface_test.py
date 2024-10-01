b732bf482f7242410f2c5029bb51a1952919be6b
Ian <ian_cruikshank@sfu.ca>
2024-10-01 16:15:25 -0700

    [MB1242] Remove commented code

--- src/tests/MB1242_interface_test.py
+++ src/tests/MB1242_interface_test.py
@@ -1,4 +1,6 @@
 
+# Must run with: sudo ~/env/bin/python MB1242_interface_test.py
+
 import time
 import sys
 
@@ -18,14 +20,6 @@ status_gpio = DigitalInputDevice(17)
 
 while(True):
     i2c_bus.write_byte(address, 81)
-
-    # is_data_ready = False
-    # while(not is_data_ready):
-    #     try:
-    #         bytes = i2c_bus.read_i2c_block_data(address, 0, 2)
-    #         is_data_ready = True
-    #     except:
-    #         time.sleep(0.01)
 
     while(status_gpio.is_active):
         time.sleep(0.01)
