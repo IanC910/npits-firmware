27c0220804f28d11392abe8060a702be6ffb76ac
Ian <ian_cruikshank@sfu.ca>
2024-10-23 00:35:29 -0700

    Add radar serial port to pin_defines

--- cpp/pin_defines.h
+++ cpp/pin_defines.h
@@ -2,8 +2,11 @@
 #ifndef PIN_DEFINES_H
 #define PIN_DEFINES_H
 
+// MB1242 ultrasonic sensor
 const int ULTRASONIC_STATUS_GPIO_NUM = 17;
 const char ULTRASONIC_I2C_DEVICE[] = "/dev/i2c-1";
 
+// OPS241 radar
+const char RADAR_SERIAL_PORT[] = "/dev/ttyACM0";
 
 #endif
