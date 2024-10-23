6366eec80f12f6a4e02b4d5a845e65855d1bc427
Ian <ian_cruikshank@sfu.ca>
2024-10-22 22:24:29 -0700

    Add GPIO_simple_test

---
+++ cpp/simple_tests/GPIO_simple_test.cpp
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#include <gpiod.h>
+
+int main() {
+    // GPIO
+    struct gpiod_chip *chip;
+    const char *chipname = "gpiochip4";
+    chip = gpiod_chip_open_by_name(chipname);
+
+    printf("Opened GPIO chip\n");
+
+    int test_gpio_num = 27;
+    struct gpiod_line *test_line;
+    test_line = gpiod_chip_get_line(chip, test_gpio_num);
+    gpiod_line_request_input(test_line, "test_line");
+
+    printf("Initialised GPIO\n");
+
+    while(1) {
+        int val = gpiod_line_get_value(test_line);
+        printf("%d\n", val);
+        usleep(10000);
+    }
+}
