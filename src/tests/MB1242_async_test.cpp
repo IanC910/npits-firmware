148c4c6d664397ca8827f2c667ea451619a5bf5e
Ian <ian_cruikshank@sfu.ca>
2024-11-18 19:47:29 -0800

    Make MB1242 return timestamp with each distance

--- src/tests/MB1242_async_test.cpp
+++ src/tests/MB1242_async_test.cpp
@@ -9,17 +9,19 @@ const char ULTRASONIC_I2C_DEVICE[] = "/dev/i2c-1";
 const int ULTRASONIC_STATUS_GPIO_NUM = 17;
 
 int main() {
-
     MB1242 ultrasonic(ULTRASONIC_I2C_DEVICE, ULTRASONIC_STATUS_GPIO_NUM);
 
     ultrasonic.begin_sampling();
 
     while(1) {
-        while(!ultrasonic.is_new_distance_available()) {
+        while(!ultrasonic.is_new_report_available()) {
             sleep_ms(5);
         }
 
-        int distance_cm = ultrasonic.get_latest_distance_cm();
-        printf("%lld: %3d cm\n", get_time_ms(), distance_cm);
+        MB1242::report latest_report = ultrasonic.get_latest_report();
+        printf("%lld: %3d cm\n",
+            latest_report.time_stamp_ms,
+            latest_report.distance_cm
+        );
     }
 }
--- src/tests/MB1242_direct_test.cpp
+++ src/tests/MB1242_direct_test.cpp
@@ -5,10 +5,10 @@
 
 #include "../devices/MB1242.h"
 
-int main() {
-    const char ULTRASONIC_I2C_DEVICE[] = "/dev/i2c-1";
-    const int ULTRASONIC_STATUS_GPIO_NUM = 17;
+const char ULTRASONIC_I2C_DEVICE[] = "/dev/i2c-1";
+const int ULTRASONIC_STATUS_GPIO_NUM = 17;
 
+int main() {
     MB1242 ultrasonic(ULTRASONIC_I2C_DEVICE, ULTRASONIC_STATUS_GPIO_NUM);
 
     while(1) {
@@ -21,13 +21,16 @@ int main() {
             sleep_ms(5);
         }
 
-        ultrasonic.update_distance_report();
-        int distance_cm = ultrasonic.get_latest_distance_cm();
-        if(distance_cm < 0) {
-            printf("Error: Bad read\n");
+        result = ultrasonic.update_report();
+        if(result == 0) {
+            MB1242::report latest_report = ultrasonic.get_latest_report();
+            printf("%lld: %3d cm\n",
+                latest_report.time_stamp_ms,
+                latest_report.distance_cm
+            );
         }
         else {
-            printf("%lld: %3d cm\n", get_time_ms(), distance_cm);
+            printf("Error: Couldn't update report\n");
         }
     }
 }
