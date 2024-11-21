f7333a2d697d3e4c45786b26d8f5f39ef125fabb
Ian <ian_cruikshank@sfu.ca>
2024-11-20 20:10:26 -0800

    Remove gps location and speed getters from le_server.h

--- src/bluetooth/le_server.h
+++ src/bluetooth/le_server.h
@@ -6,10 +6,4 @@ void le_server_start();
 
 void le_server_stop();
 
-double le_server_get_latest_latitude();
-
-double le_server_get_latest_longitude();
-
-double le_server_get_latest_speed_mps();
-
 #endif
