2d9ec227e073fdd795d0eadcdaf604d50d3be6cf
Ian <ian_cruikshank@sfu.ca>
2024-11-29 18:46:58 -0800

    Add function to set global time and test

--- src/common/time_tools.cpp
+++ src/common/time_tools.cpp
@@ -4,47 +4,52 @@
 
 #include "time_tools.h"
 
+// Corrective offset added to all get time functions
+static time_t time_s_offset = 0;
+
+void set_time_s(time_t time_s) {
+    time_s_offset = time_s - get_time_s();
+}
+
 struct timespec get_timespec() {
     struct timespec now;
-    clock_gettime(CLOCK_REALTIME, &now);
+    timespec_get(&now, TIME_UTC);
+    now.tv_sec += time_s_offset;
     return now;
 }
 
-long long get_time_s() {
-    struct timespec now;
-    clock_gettime(CLOCK_REALTIME, &now);
+time_t get_time_s() {
+    struct timespec now = get_timespec();
     return now.tv_sec;
 }
 
-long long get_time_ms() {
-    struct timespec now;
-    clock_gettime(CLOCK_REALTIME, &now);
-    long long milliseconds = now.tv_sec * 1000 + now.tv_nsec / 1000000;
+time_t get_time_ms() {
+    struct timespec now = get_timespec();
+    time_t milliseconds = now.tv_sec * 1000 + now.tv_nsec / 1000000;
     return milliseconds;
 }
 
-long long get_time_us() {
-    struct timespec now;
-    clock_gettime(CLOCK_REALTIME, &now);
-    long long microseconds = now.tv_sec * 1000000 + now.tv_nsec / 1000;
+time_t get_time_us() {
+    struct timespec now = get_timespec();
+    time_t microseconds = now.tv_sec * 1000000 + now.tv_nsec / 1000;
     return microseconds;
 }
 
-void sleep_s(long long sleep_time_s) {
+void sleep_s(time_t sleep_time_s) {
     struct timespec sleep_time = {sleep_time_s, 0};
     nanosleep(&sleep_time, NULL);
 }
 
-void sleep_ms(long long sleep_time_ms) {
-    long long sleep_time_ns = sleep_time_ms * 1000000;
+void sleep_ms(time_t sleep_time_ms) {
+    time_t sleep_time_ns = sleep_time_ms * 1000000;
     int s = sleep_time_ns / 1000000000;
     int ns = sleep_time_ns % 1000000000;
     struct timespec sleep_time = {s, ns};
     nanosleep(&sleep_time, NULL);
 }
 
-void sleep_us(long long sleep_time_us) {
-    long long sleep_time_ns = sleep_time_us * 1000;
+void sleep_us(time_t sleep_time_us) {
+    time_t sleep_time_ns = sleep_time_us * 1000;
     int s = sleep_time_ns / 1000000000;
     int ns = sleep_time_ns % 1000000000;
     struct timespec sleep_time = {s, ns};
