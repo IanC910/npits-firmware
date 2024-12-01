e5256e016a2fdde348fe6b7e01e96f6db5484d4d
Ian <ian_cruikshank@sfu.ca>
2024-11-30 16:46:05 -0800

    Add get timestamp function

--- src/tests/time_test.cpp
+++ src/tests/time_test.cpp
@@ -1,16 +1,23 @@
 
 #include <stdio.h>
+#include <string>
 
 #include "../common/time_tools.h"
 
 int main() {
     printf("Now: %ld\n", get_time_s());
+    printf("%s\n", get_timestamp_hms().c_str());
     sleep_s(2);
     printf("2s later: %ld\n", get_time_s());
+    printf("%s\n", get_timestamp_hms().c_str());
 
     set_time_s(0);
 
     printf("Now: %ld\n", get_time_s());
+    printf("%s\n", get_timestamp_hms().c_str());
     sleep_s(2);
     printf("2s later: %ld\n", get_time_s());
+    printf("%s\n", get_timestamp_hms().c_str());
+
+
 }
