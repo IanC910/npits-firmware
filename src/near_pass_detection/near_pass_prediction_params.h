1a653d77ee26c1307a71a6128a3669efc1499f1e
Ian <ian_cruikshank@sfu.ca>
2024-12-02 00:06:31 -0800

    Change nonsensical range prediction params

--- src/near_pass_detection/near_pass_prediction_params.h
+++ src/near_pass_detection/near_pass_prediction_params.h
@@ -5,8 +5,8 @@
 const float MIN_SPEED_mps = 6;
 const float MIN_SPEED_MAGNITUDE = 25;
 
-const float MIN_RANGE_m = 2.5;
-const float MAX_RANGE_m = 2.0;
+const float MIN_RANGE_m = 2.0;
+const float MAX_RANGE_m = 3.0;
 const float MIN_RANGE_MAGNITUDE = 300;
 
 #endif
