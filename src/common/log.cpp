da5e329da32228b5ee4f48b8ac784d6198a76c84
Ian <ian_cruikshank@sfu.ca>
2024-12-01 21:55:07 -0800

    Add periodic saving to log module

--- src/common/log.cpp
+++ src/common/log.cpp
@@ -6,7 +6,11 @@
 
 #include "log.h"
 
+const int SAVE_PERIOD_ms = 100;
+
 static FILE* log_file = nullptr;
+
+static long long time_of_last_save_ms = 0;
 
 void log(std::string tag, std::string message) {
     if(log_file == nullptr) {
@@ -20,4 +24,11 @@ void log(std::string tag, std::string message) {
 
     printf("%s\n", text.c_str());
     fprintf(log_file, "%s\n", text.c_str());
+
+    long long now_ms = get_time_ms();
+    if(now_ms > time_of_last_save_ms + SAVE_PERIOD_ms) {
+        fclose(log_file);
+        log_file = fopen(LOG_FILE_NAME.c_str(), "a");
+        time_of_last_save_ms = now_ms;
+    }
 }
