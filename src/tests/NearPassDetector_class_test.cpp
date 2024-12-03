65f1c184fbffece303a45de326baeb5dfb4a759e
Ian <ian_cruikshank@sfu.ca>
2024-12-03 13:49:07 -0800

    Fix Detector class test

--- src/tests/NearPassDetector_class_test.cpp
+++ src/tests/NearPassDetector_class_test.cpp
@@ -7,7 +7,7 @@
 
 int main() {
     MB1242 ultrasonic(ULTRASONIC_I2C_DEVICE, ULTRASONIC_STATUS_GPIO_NUM);
-    NearPassDetector near_pass_detector(&ultrasonic, nullptr);
+    NearPassDetector near_pass_detector(&ultrasonic);
 
     // Blocking, no stop condition except ctrl+c
     near_pass_detector.run();
