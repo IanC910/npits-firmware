1abdb9233b6b3e9924ef71e8deca5920314e5aff
Ian <ian_cruikshank@sfu.ca>
2024-12-02 17:33:22 -0800

    add some near pass data and a graceful exit for sensor coord test

--- src/tests/sensor_coordination_test.cpp
+++ src/tests/sensor_coordination_test.cpp
@@ -1,6 +1,10 @@
 
 #include <string>
 #include <thread>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <iostream>
 
 #include "../connection_params.h"
 
@@ -9,6 +13,31 @@
 
 #include "../near_pass_detection/NearPassPredictor.h"
 #include "../near_pass_detection/NearPassDetector.h"
+
+int kbhit() {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if (ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
 
 
 int main() {
@@ -22,5 +51,20 @@ int main() {
     near_pass_predictor.start();
 
     // Run this one here, blocking. No stop condition except ctrl+c
-    near_pass_detector.run();
+    near_pass_detector.start();
+
+    while(true) {
+        if (kbhit()) {
+            char ch = getchar();
+            if (ch == 'q' || ch == 'Q') {
+                std::cout << "Exiting program..." << std::endl;
+                break;
+            }
+        }
+    }
+
+    near_pass_detector.stop();
+    near_pass_predictor.stop();
+
+
 }
