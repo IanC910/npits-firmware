f73358f6ac9d3b688197b564d9ccdeac02569889
Ian <ian_cruikshank@sfu.ca>
2024-12-04 01:00:06 -0800

    Edit some prediction params

--- src/near_pass_detection/near_pass_prediction_params.h
+++ src/near_pass_detection/near_pass_prediction_params.h
@@ -2,10 +2,10 @@
 #ifndef NEAR_PASS_PREDICTION_PARAMS_H
 #define NEAR_PASS_PREDICTION_PARAMS_H
 
-const float MIN_SPEED_mps = 3.0;
+const float MIN_SPEED_mps = 2;
 const float MAX_SPEED_mps = 30.0;
 
-const int MIN_SPEED_MAGNITUDE = 25;
+const int MIN_SPEED_MAGNITUDE = 20;
 
 const float MIN_RANGE_m = 2.0;
 const float MAX_RANGE_m = 20.0;
